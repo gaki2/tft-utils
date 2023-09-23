d90c17e0364777f38247260c319bbaf9ac985bf5
유병각 <bgyu@miridih.com>
2023-09-23 13:18:04 +0900

    feat: useBoard.ts 훅 생성
    - rerender 가 있으며, board 를 리턴한다.

--- src/components/board/useBoard.ts
+++ src/components/board/useBoard.ts
@@ -1,9 +1,17 @@
-import {useMemo, useState} from "react";
-import {Board, ChampionData} from './model/Board';
+import {useEffect, useMemo, useState} from "react";
+import {Board, ChampionData} from './class/Board';
 
 export const useBoard = (championData: ChampionData[]) => {
   const [rerender, setRerender] = useState(0);
   const board = useMemo(() =>  new Board(championData), [])
+
+  useEffect(() => {
+    if (board) {
+      board.addSlotsStateListener(() => {
+        setRerender((prev) => prev + 1)
+      })
+    }
+  }, [board]);
 
   return {
     board,
