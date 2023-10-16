fff18b37fb5be07bc4b0bd2cc4e2df8f41e92240
유병각 <bgyu@miridih.com>
2023-10-17 00:25:36 +0900

    feat: 기존 Tooltip 컴포넌트의 구조를 재사용하기 위해서 TFT_TOOLTIP 컴포넌트 생성

--- src/utils/components/ReactTooltip.tsx
+++
@@ -1,8 +0,0 @@
-import { Tooltip } from 'react-tooltip';
-import React from 'react';
-
-type ReactTooltipProps = React.ComponentProps<typeof Tooltip>;
-
-export const ReactTooltip = (props: ReactTooltipProps) => {
-  return <Tooltip delayShow={0} delayHide={0} {...props} />;
-};
