9bc3dcd4a99d5e6365f7f8ea53744cf2d1d0d86c
유병각 <bgyu@miridih.com>
2023-11-02 20:02:33 +0900

    feat: parser 에 정규표현식 관련 함수를 regex 파일로 이동

--- src/utils/regex.ts
+++ src/utils/regex.ts
@@ -1,3 +1,17 @@
 export const underBarToSpace = (str: string) => str.replace(/_/g, ' ');
 
-export const SpaceToUnderBar = (str: string) => str.replace(/ /g, '_');
+export const spaceToUnderBar = (str: string) => str.replace(/ /g, '_');
+
+/**
+ * 문자열에 포함된 `<br>` 을 제거함.
+ */
+export const cleanBrTag = (str: string) => {
+  return str.replace(/<br>/g, '');
+};
+
+/**
+ * <br> 태그를 제외한 <~~~>, </~~~> 형태의 태그를 제거함.
+ */
+export const cleanAngleBracket = (str: string) => {
+  return str.replace(/<(?!br>)[^>]*>/g, '');
+};
