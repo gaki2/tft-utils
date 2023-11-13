77f7dab79e576bb1c08cec8818d3c4c990566b3f
유병각 <bgyu@miridih.com>
2023-11-13 22:50:28 +0900

    feat: 챔피언 게터, 파서 수정

--- src/getter/champion_getter.ts
+++ src/getter/champion_getter.ts
@@ -1,83 +1,28 @@
-import { LanguageType, Season } from '../types';
-import {
-  ChampionName_season_9_ko,
-  champions_season_9_ko,
-} from '../_generated/season_9/champion_ko';
-import {
-  ChampionName_season_9b_ko,
-  champions_season_9b_ko,
-} from '../_generated/season_9b/champion_ko';
-import {
-  ChampionName_season_9_en,
-  champions_season_9_en,
-} from '../_generated/season_9/champion_en';
-import {
-  ChampionName_season_9b_en,
-  champions_season_9b_en,
-} from '../_generated/season_9b/champion_en';
-import { Champion } from '../script/parser/championParser';
+import { Season } from '../types';
+import { ChampionData_10, champions_season_10 } from '../_generated/season_10/champions_season_10';
 
-type ChampionDataSetMap = {
-  season_9: {
-    ko: typeof champions_season_9_ko;
-    en: typeof champions_season_9_en;
-  };
-  season_9b: {
-    ko: typeof champions_season_9b_ko;
-    en: typeof champions_season_9b_en;
-  };
-};
+export class ChampionGetter {
+  /**
+   * 이 함수는 인터페이스에 노출되는 함수가 아니라서, name 의 타입을 강력하게 제한하지 않았다.
+   * 강력하게 제한 한다는 뜻은, name 의 타입을 챔피언 이름의 유니온 타입이 되도록 한다는 것이다.
+   *
+   * 제한을 느슨하게 함으로써, 아래 함수는 어디에서든지 사용될 수 있다는 이점이 있다.
+   * 타입에러를 추론하지 못하는 만큼 리턴 타입에 명시적으로 nullable 을 사용하여 사용처에서 예외처리를 하도록 만들었다.
+   */
+  static getChampionDataFromName(name: string, season: Season) {
+    switch (season) {
+      case 'season_10':
+        const target = (
+          Object.keys(champions_season_10) as (keyof typeof champions_season_10)[]
+        ).find((apiName) => {
+          return apiName === `TFT10_${name}`;
+        });
 
-type ChampionNameMap = {
-  season_9: {
-    ko: ChampionName_season_9_ko;
-    en: ChampionName_season_9_en;
-  };
-  season_9b: {
-    ko: ChampionName_season_9b_ko;
-    en: ChampionName_season_9b_en;
-  };
-};
+        if (!target) {
+          return null;
+        }
 
-type ChampionDataSet<S extends Season, L extends LanguageType> = ChampionDataSetMap[S][L];
-export type ChampionName<S extends Season, L extends LanguageType> = ChampionNameMap[S][L];
-
-export class ChampionGetter<S extends Season, L extends LanguageType> {
-  constructor(private season: S, private language: L) {}
-
-  getChampionDataSet(): ChampionDataSet<S, L> {
-    switch (this.season) {
-      case 'season_9':
-        return this.language === 'ko' ? champions_season_9_ko : champions_season_9_en;
-      case 'season_9b':
-        return this.language === 'ko' ? champions_season_9b_ko : champions_season_9b_en;
-      default:
-        throw Error(
-          '에러발생 위치: getChampionDataSet() / 에러내용: 아이템 데이터셋을 불러오는데 실패하였습니다.'
-        );
+        return champions_season_10[target] as ChampionData_10;
     }
-  }
-
-  getAllChampionNames(): ChampionName<S, L>[] {
-    const dataSet = this.getChampionDataSet();
-    return Object.values(dataSet).map((champion) => champion.name as ChampionName<S, L>);
-  }
-
-  getDataFromName(name: ChampionName<S, L>): Champion {
-    const dataSet = this.getChampionDataSet();
-
-    for (const [key, value] of Object.entries(dataSet)) {
-      if (value.name === name) {
-        return dataSet[key as keyof typeof dataSet] as Champion;
-      }
-    }
-
-    return {
-      apiName: '',
-      name: '',
-      url: '',
-      cost: 0,
-      traits: [],
-    };
   }
 }
--- src/script/parser/championParser.ts
+++ src/script/parser/championParser.ts
@@ -1,102 +1,163 @@
 import { LanguageType, Season } from '../../types';
-import { S3 } from '../../environments/urls';
-import { GeneralParser } from './generalParser';
-import { removeQuote, spaceToUnderBar } from '../../utils/regex';
-import { SEASON_SET_DATA_IDX_MAP } from '../shared';
-import { removeDuplicates } from '../../utils/my_lodash';
+import { SEASON_SET_DATA_IDX_MAP } from '../SET_DATA_IDX';
+import { General } from '../general';
+import _ from 'lodash';
+import path from 'path';
 
-export type Champion = {
-  /**
-   * @example
-   * Language 가 한국어일때는, '징크스'
-   * 영어일때는 'Jinx'
-   */
-  name: string;
-  apiName: string;
-  url: string;
-  cost: number;
-  traits: string[];
-};
+const BRANCHS_MAP = {
+  season_10: ['ability.desc', 'ability.name', 'name', 'traits'],
+} satisfies Record<Season, string[]>;
 
-export class ChampionParser {
-  constructor(private language: LanguageType, private season: Season) {}
+const jsonDir = path.join(__dirname, '../../json');
+const generatedDir = path.join(__dirname, '../../_generated');
 
-  parseChampionList(championListJsonString: string) {
-    const championList = JSON.parse(championListJsonString).data;
-
-    if (Object.keys(championList).length === 0) {
-      throw new Error('championList is empty');
-    }
-
-    return Object.values<{ id: string; tier: number; name: string }>(championList).map((value) => {
-      const { id } = value;
-      return {
-        id,
-      };
-    });
-  }
-
-  parseChampionData(
-    championDataJsonString: string,
-    championList: ReturnType<typeof this.parseChampionList>
-  ) {
-    const championData =
-      JSON.parse(championDataJsonString)['setData'][SEASON_SET_DATA_IDX_MAP[this.season]].champions;
-
-    if (Object.keys(championData).length === 0) {
-      throw new Error('championData is empty');
-    }
-
-    return championList.reduce((acc, { id }) => {
-      const data = championData.find((object: { apiName: string }) => object.apiName === id);
-
-      if (!data) {
-        console.error(`${id} is not matched!`);
-        return acc;
-      }
-
-      const { name: originalName, traits, cost } = data;
-
-      const apiName = parseApiName(id);
-      const name = GeneralParser.applyRegex(originalName, spaceToUnderBar, removeQuote);
-      acc[id] = {
-        apiName: id,
-        name,
-        url: `${S3}/${this.season}/champions/${apiName}.png`,
-        cost,
-        traits: traits.map((trait: string) => GeneralParser.applyRegex(trait, spaceToUnderBar)),
-      };
-
-      return acc;
-    }, {} as { [key in string]: Champion });
-  }
-
-  parseChampionName(
-    championDataJsonString: string,
-    championList: ReturnType<typeof this.parseChampionList>
-  ) {
-    const championData =
-      JSON.parse(championDataJsonString)['setData'][SEASON_SET_DATA_IDX_MAP[this.season]].champions;
-
-    return removeDuplicates(
-      championList
-        .map(({ id }) => {
-          const data = championData.find((object: { apiName: string }) => object.apiName === id);
-          if (!data) {
-            return '';
-          }
-          const { name: originalName } = data;
-          return GeneralParser.applyRegex(originalName, spaceToUnderBar, removeQuote);
-        })
-        .filter(Boolean)
-    );
+function getAdditionalText(season: Season) {
+  switch (season) {
+    case 'season_10':
+      return `export type ChampionName_10 = Split<keyof typeof champions_season_10, '_'>[1];
+export type ChampionData_10 = DeepNullable<(typeof champions_season_10)['TFT10_Ahri']>;
+`;
+    default:
+      return '';
   }
 }
 
-/**
- * @example
- * handleException('TFT9_Zeus') // 'Zeus'
- */
-const parseApiName = (_apiName: string) => {
-  return _apiName.split('_')[1];
-};
+export class ChampionParser {
+  static extractChampionData(season: Season, language: LanguageType) {
+    /**
+     * @returns
+     * {
+     *   'TFT9_Aatrox': {
+     *    'name': '아트록스',
+     *    'cost': 3,
+     *    'traits': ['척후병', '천상'],
+     *   },
+     *   'TFT9_Ahri': {
+     *    'name': '아리',
+     *    'cost': 5,
+     *    'traits': ['신비술사', '천상'],
+     *   ...
+     * } 과 같은 형태이다.
+     */
+    const dataSet = General.readFileSync(`${jsonDir}/${season}/tft_data_${language}.json`);
+    const parsedDataSet = General.parse(dataSet);
+    /**
+     * championData 는 다음과 같은 형태로 들어있다.
+     * [
+     *   {
+     *     "apiName": "TFT9_Aatrox",
+     *     "name": "아트록스",
+     *     "cost": 3, ...
+     *   },
+     *   {
+     *     "apiName": "TFT9_Ahri",
+     *     "name": "아리",
+     *     "cost": 5, ...
+     *   }...
+     * ]
+     */
+    const raw_championData = parsedDataSet['setData'][SEASON_SET_DATA_IDX_MAP[season]].champions;
+
+    const parsed_championData = raw_championData.reduce((acc: any, champion: any) => {
+      const apiName = champion.apiName;
+      acc[apiName] = champion;
+      return acc;
+    }, {} as { [key: string]: any });
+
+    return General.writeFile(
+      `${jsonDir}/${season}/champion_data_${language}.json`,
+      JSON.stringify(parsed_championData, null, 2)
+    );
+  }
+
+  static mergeChampionData(season: Season, languages: LanguageType[]) {
+    /**
+     * championDataMap 의 구조
+     * {
+     *   'ko': {
+     *     'TFT9_Aatrox': {
+     *       'name': '아트록스',
+     *       'cost': 3, ...
+     *   },
+     *   'en': {
+     *     'TFT9_Aatrox': {
+     *      'name': 'Aatrox',
+     *      'cost': 3, ...
+     *   }
+     * }
+     */
+    const championDataMap = languages.reduce((acc, language) => {
+      const championDataSet = General.readFileSync(
+        `${jsonDir}/${season}/champion_data_${language}.json`
+      );
+      const parsedChampionDataSet = General.parse(championDataSet);
+
+      acc[language] = parsedChampionDataSet;
+      return acc;
+    }, {} as { [key in LanguageType]: any });
+    /**
+     * pointMap 의 구조
+     * {
+     *     'TFT9_Aatrox': {
+     *       'name': '아트록스',
+     *       'cost': 3, ...,
+     *     'TFT9_Ahri': {
+     *     'name': '아리',
+     *     'cost': 5, ...
+     *     }
+     *   }
+     */
+    const pointMap = championDataMap.ko;
+    const apiNames = Object.keys(pointMap);
+    const seasonNumber = season.split('_')[1];
+
+    const merged_data = apiNames.reduce((acc, apiName) => {
+      /**
+       * apiName 이 TFT${seasonNumber} 로 시작하지 않으면 무시한다.
+       */
+      if (!apiName.includes(`TFT${seasonNumber}`)) {
+        return acc;
+      }
+
+      const pointData = pointMap[apiName];
+      const merged_champion = _.cloneDeep(pointData);
+
+      const branchs = BRANCHS_MAP[season];
+      // change original data to empty object
+      branchs.forEach((branch) => {
+        _.set(merged_champion, branch, {});
+      });
+
+      languages.forEach((language) => {
+        const nowData = championDataMap[language][apiName];
+        branchs.forEach((branch) => {
+          _.set(merged_champion, branch + `.${language}`, _.get(nowData, branch));
+        });
+      });
+
+      acc[apiName] = merged_champion;
+      return acc;
+    }, {} as { [key: string]: any });
+
+    const ret = `export const champions_${season} = ${JSON.stringify(merged_data, null, 2)}
+
+${getAdditionalText(season)}
+  `;
+
+    return General.writeFile(`${generatedDir}/${season}/champions_${season}.ts`, ret);
+  }
+}
+
+async function parseChampion(season: Season) {
+  const languages: LanguageType[] = ['ko', 'en'];
+
+  const promises = languages.map((language) =>
+    ChampionParser.extractChampionData(season, language)
+  );
+
+  await Promise.all(promises);
+
+  await ChampionParser.mergeChampionData(season, languages);
+}
+
+parseChampion('season_10');
