4d2115ac9ad26660736e510531e8c685cb317a31
유병각 <bgyu@miridih.com>
2023-11-13 22:50:56 +0900

    feat: 유틸함수 추가

--- src/script/parser/generalParser.ts
+++ src/script/parser/generalParser.ts
@@ -1,5 +1,8 @@
 import { convertKeysToUpperCase, isEmptyObject } from '../../utils/my_lodash';
 import { General } from '../general';
+import path from 'path';
+
+const jsonDir = path.join(__dirname, '../../json');
 
 export class GeneralParser extends General {
   static replaceVariables(text: string, variables: any) {
@@ -14,4 +17,12 @@ export class GeneralParser extends General {
       return match;
     });
   }
+
+  /**
+   * cdragon 에서 받아온 json 파일이 UTF-8 로 인코딩 되어 있는 경우가 있어서, (한국어파일에서)
+   * 그 경우에 해당 json 파일을 디코딩하여 temp 폴더에 temp.json 으로 저장한다.
+   */
+  static UTF8ToKoJson = (jsonString: string) => {
+    General.writeFile(`${jsonDir}/temp/temp.json`, JSON.stringify(JSON.parse(jsonString), null, 2));
+  };
 }
