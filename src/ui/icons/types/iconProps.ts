b9629e0f77ab959d66597d37f43dae5c430fbde0
유병각 <bgyu@miridih.com>
2023-12-18 11:30:44 +0900

    feat: svgr 세팅 및 UI 폴더 생성

---
+++ src/ui/icons/types/iconProps.ts
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export type IconProps = {
+  /**
+   * @default 1.5rem
+   */
+  width?: React.CSSProperties['width'];
+  /**
+   * @default 1.5rem
+   */
+  height?: React.CSSProperties['height'];
+  color?: React.CSSProperties['color'];
+};
